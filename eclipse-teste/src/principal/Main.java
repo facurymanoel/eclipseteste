3d73ae0bdf22a294b8edca22fe8ade340c0b17f1
dalmo <facurymanoel10@gmail.com>
2024-01-12 15:05:16 -0300

    Criando a classe Principal

--- eclipse-teste/src/principal/Main.java
+++ eclipse-teste/src/principal/Main.java
@@ -1,10 +1,15 @@
 package principal;
 
+import teste.Cliente;
+
 public class Main {
 
 	public static void main(String[] args) {
 		 
-
+		  Cliente cliente = new Cliente("James", 30);
+		  
+		 System.out.println(cliente.getNome());
+ 
 	}
 
 }
