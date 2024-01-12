d4c974e4f9fb7ccbad8a96a0baa3595f2343eeeb
dalmo <facurymanoel10@gmail.com>
2024-01-12 13:55:20 -0300

    Acrescentando os getts e setters

--- eclipse-teste/src/teste/Cliente.java
+++ eclipse-teste/src/teste/Cliente.java
@@ -4,5 +4,19 @@ public class Cliente {
 	
 	String nome;
 	int idade;
+	public String getNome() {
+		return nome;
+	}
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+	public int getIdade() {
+		return idade;
+	}
+	public void setIdade(int idade) {
+		this.idade = idade;
+	}
+	
+	
 
 }
