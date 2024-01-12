7c72ae94a3d118439cd6ddea7850445805adb152
dalmo <facurymanoel10@gmail.com>
2024-01-12 14:23:22 -0300

    Adicionando o construtor na classe Cliente

--- eclipse-teste/src/teste/Cliente.java
+++ eclipse-teste/src/teste/Cliente.java
@@ -10,10 +10,17 @@ public class Cliente {
 	public void setNome(String nome) {
 		this.nome = nome;
 	}
+	
 	public int getIdade() {
 		return idade;
 	}
 	public void setIdade(int idade) {
+		this.idade = idade;
+	}
+	
+	public Cliente(String nome, int idade) {
+		super();
+		this.nome = nome;
 		this.idade = idade;
 	}
 	
